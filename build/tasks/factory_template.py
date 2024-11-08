0e58274cd215592c72036a760b5ce4ad28964296
userbrians <brians018883@gmail.com>
2024-11-07 23:59:42 -0800

    remove unnecessary @property functions

--- build/tasks/factory_template.py
+++ build/tasks/factory_template.py
@@ -1,59 +1,33 @@
-from abc import abstractmethod
+from abc import ABC, abstractmethod
 from subprocess import run
 
 from build import decorators
 
-from re import sub
 
+class TaskFactoryTemplate(ABC):
+    comment_string: str = ""
 
-class TaskFactoryTemplate:
-    _index: int = 0
-    _title: str = ""
-    _command_string: str = ""
-    _comment: str = ""
-
+    @property
     @abstractmethod
-    def __post_init__(self) -> None:
+    def index(self) -> int:
         raise NotImplementedError()
 
     @property
-    def index(self) -> int:
-        return self._index
-
-    @index.setter
-    def index(self, integer: int) -> None:
-        self._index = integer
-
-    @property
+    @abstractmethod
     def title(self) -> str:
-        return self._title
-
-    @title.setter
-    def title(self, name: str) -> None:
-        self._title = name
+        raise NotImplementedError()
 
     @property
+    @abstractmethod
     def command_string(self) -> str:
-        return self._command_string
-
-    @command_string.setter
-    def command_string(self, name: str) -> None:
-        self._command_string = name
-
-    @property
-    def comment(self) -> str:
-        return self._comment
-
-    @comment.setter
-    def comment(self, name: str) -> None:
-        self._comment = name
+        raise NotImplementedError()
 
     @decorators.DoublePaddedFooterWrapper(message="Finished")
     def perform_task(self) -> None:
         self.index_and_title()
         self.return_command_string()
         self.execute_command_string()
-        if self.comment:
+        if self.comment_string:
             self.after_comment()
 
     @decorators.ColorizedIndentPrinter(indent=1, end=":", style="task")
@@ -73,4 +47,4 @@ class TaskFactoryTemplate:
 
     @decorators.ColorizedIndentPrinter(indent=2, begin="", end="", style="task")
     def after_comment(self) -> str:
-        return f"{self.comment}"
+        return f"{self.comment_string}"
