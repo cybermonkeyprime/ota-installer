00b3bae31e3105502d0dc6d4a09c9ba30bf2e00b
userbrians <brians018883@gmail.com>
2024-11-07 18:18:02 -0800

    use @property for version_info

--- build/software_versioning/detailed_verion.py
+++ build/software_versioning/detailed_verion.py
@@ -5,10 +5,9 @@ from .base_version import BaseVersion
 
 @dataclass
 class DetailedVersion(BaseVersion):
-    version_info: str = field(default="", init=False)
-
-    def __post_init__(self) -> None:
-        self.version_info = (
+    @property
+    def version_info(self) -> str:
+        return (
             f"{self.title} Build: {self.build_number}, Revision: {self.revision_number}"
         )
 
