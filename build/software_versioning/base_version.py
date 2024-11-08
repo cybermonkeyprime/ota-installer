409d5696a9758c1a2237ff0d795c9090a029a7be
userbrians <brians018883@gmail.com>
2024-11-07 18:24:59 -0800

    updated to 20241107-10070620

--- build/software_versioning/base_version.py
+++ build/software_versioning/base_version.py
@@ -4,5 +4,5 @@ from dataclasses import dataclass
 @dataclass
 class BaseVersion(object):
     title: str = "OTA-Installer"
-    build_number: int = 20241005
-    revision_number: int = 10050445
+    build_number: int = 20241107
+    revision_number: int = 10070620
