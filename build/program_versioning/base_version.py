d286dd49b1db34b0fbc5932e70a54d8436d1b73b
userbrians <brians018883@gmail.com>
2025-03-28 02:01:58 -0700

    Upped Version number to 20250328.280130

--- build/program_versioning/base_version.py
+++ build/program_versioning/base_version.py
@@ -4,5 +4,9 @@ from dataclasses import dataclass
 @dataclass
 class BaseVersion(object):
     title: str = "OTA-Installer"
-    build_number: int = 20250221
-    revision_number: int = 2220430
+    build_number: int = 20250328  # YYYYMMDD
+    revision_number: int = 280130  # ddhhmm
+
+    @property
+    def tag(self) -> str:
+        return f"v{self.build_number}.{self.revision_number}"
