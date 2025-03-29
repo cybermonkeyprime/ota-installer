3daf50e6e1945b358cacdaca93b1d2cd22b83ac0
userbrians <brians018883@gmail.com>
2025-03-29 03:11:05 -0700

    renamed variable_managers.py to variable_item_managersdispatcher_manager.py

--- build/variables/managers/__init__.py
+++ build/variables/managers/__init__.py
@@ -1,1 +1,6 @@
-from .variable_managers import *
+from .boot_image_manager import *
+from .dispatcher_manager import *
+from .file_name_manager import *
+from .log_file_manager import *
+from .patched_image_manager import *
+from .variable_item_managers import *
