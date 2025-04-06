8812b7da8d4d5cdc17fbb39f1b653ea8007122b4
userbrians <brians018883@gmail.com>
2025-04-06 04:03:02 -0700

    Changed patched_image_name to use MagiskImageVariableManager

--- build/variables/variable_manager.py
+++ build/variables/variable_manager.py
@@ -2,13 +2,18 @@ from dataclasses import dataclass, field
 from pathlib import Path
 from typing import Optional
 
-import build.dispatchers as dispatchers
-import build.structures as structures
-import build.types.definitions as type_definitions
-import build.types.managers as type_managers
-import build.variables.managers as variable_managers
-
-magisk = structures.MagiskStruct()
+from build.components.boot_image import BootImageVariableManager
+from build.components.directory.types import (
+    DirectoryTypeDefinition,
+    DirectoryTypeManager,
+)
+from build.components.file.variables import FileNameVariableManager
+from build.components.log_file import LogFileManager
+from build.components.magisk_image.variables import MagiskImageVariableManager
+from build.dispatchers import MainDispatcher
+from build.variables.managers import (
+    DispatcherManager,
+)
 
 
 @dataclass
@@ -18,40 +23,37 @@ class VariableManager(object):
     file_path: Path = field(default_factory=Path)
 
     @property
-    def file_name(self) -> "variable_managers.FileNameManager":
-        return variable_managers.FileNameManager(path=self.file_path)
-
-    def file_name_manager(self) -> "variable_managers.FileNameManager":
-        return variable_managers.FileNameManager(path=self.file_path)
+    def file_name(self) -> FileNameVariableManager:
+        return FileNameVariableManager(path=self.file_path)
 
     @property
-    def boot_image(self) -> "variable_managers.BootImageManager":
-        return variable_managers.BootImageManager(file_path=self.file_name.validator())
+    def boot_image(self) -> BootImageVariableManager:
+        return BootImageVariableManager(file_path=self.file_name.validator())
 
     @property
     def patched_image_name(self) -> str:
-        patched_image_manager = variable_managers.PatchedImageManager()
-        return patched_image_manager.image_name
+        magisk_image_variable_manager = MagiskImageVariableManager()
+        return magisk_image_variable_manager.image_name
 
     @patched_image_name.setter
     def patched_image_name(self, image_name: str) -> bool:
-        patched_image_creator = variable_managers.PatchedImageManager(
+        magisk_image_variable_manager = MagiskImageVariableManager(
             image_name=image_name
         )
-        self.patched_image_name = patched_image_creator.image_name
+        self.patched_image_name = magisk_image_variable_manager.image_name
         return True
 
     @property
-    def log_file(self) -> "variable_managers.LogFileManager":
-        return variable_managers.LogFileManager(parser=self.file_name.parser())
+    def log_file(self) -> LogFileManager:
+        return LogFileManager(parser=self.file_name.parser())
 
     @property
-    def directory(self) -> Optional[type_definitions.Directory]:
-        directory_manager = type_managers.Directory(self.file_name.path.parent)
+    def directory(self) -> DirectoryTypeDefinition | None:
+        directory_manager = DirectoryTypeManager(Path.home())
         return directory_manager.create_directory()
 
-    def get_dispatcher(self, object_type: str) -> Optional[dispatchers.MainDispatcher]:
-        dispatcher_manager = variable_managers.DispatcherManager(
+    def get_dispatcher(self, object_type: str) -> Optional[MainDispatcher]:
+        dispatcher_manager = DispatcherManager(
             dispatcher_class=VariableManager, base_path=self.file_path
         )
         return dispatcher_manager.creator(object_type)
