b8030df58892f15039f49c425198c2efc98e8753
userbrians <brians018883@gmail.com>
2025-04-18 01:47:15 -0700

    Organize imports, introduce property magisk_remote_path, remove strip('\n') from checkout result

--- build/tasks/components/t07_magisk_image_finder.py
+++ build/tasks/components/t07_magisk_image_finder.py
@@ -1,15 +1,14 @@
 from dataclasses import dataclass, field
 from subprocess import CalledProcessError, check_output
 
-import build.variables as variables
-import build.tasks as tasks
-
 from decorators import (
-    ContinueOnKeyPress,
     ConfirmationPrompt,
+    ContinueOnKeyPress,
 )
 
 import build.decorators as decorators
+import build.tasks as tasks
+import build.variables as variables
 
 
 @dataclass
@@ -25,16 +24,20 @@ class MagiskImageFinder(tasks.TaskFactoryTemplate):
         return "Find Magisk Image"
 
     @property
+    def magisk_remote_path(self) -> type:
+        return self.instance.directory.magisk_image_path.remote_path
+
+    @property
     def command_string(self) -> str:
-        return f"adb shell ls { self.instance.directory.magisk_image.remote_path} | grep magisk_patched | head -n1"
+        return f"adb shell ls {self.magisk_remote_path}/magisk_patched*.img | head -n1"
 
     @ConfirmationPrompt(comment="Execute the command", indent=2, char=" ")
     @ContinueOnKeyPress(indent=1, char=" ")
     @decorators.Encapsulate()
     def execute_command_string(self) -> None:
         try:
-            result = check_output(self.command_string, shell=True).decode().strip("\n")
-            print(f"{4 * " "}Patched Boot Image = {result}")
+            result = check_output([self.command_string], shell=True).decode()
+            print(f"{4 * ' '}Patched Boot Image = {result}")
             self.instance.patched_image_name = result
         except CalledProcessError as e:
             print(f"An error occurred while executing the command: {e}")
