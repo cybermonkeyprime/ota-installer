1ea95612aa3051691dd5d277e5d9c604bc5cb985
userbrians <brians018883@gmail.com>
2025-05-14 03:30:15 -0700

    Refactor file_iteration_processor.py: remove unused imports on lines 2-4, change import alias on line 6, modify iterate_files method signature and logic on lines 11-14, update FileIterationProcessor class and remove dispatch_handler property on lines 17-36, add IndividualFileProcessor class on lines 38-58, and update process_individual_file method on lines 60-75.

--- build/display/processors/file_iteration_processor.py
+++ build/display/processors/file_iteration_processor.py
@@ -1,9 +1,6 @@
 from dataclasses import dataclass, field
-from typing import Tuple
 
-from dispatchers import DispatcherManager
-
-import build.display.base_classes as display_base_classes
+import build.display.base_classes as dbc
 import build.variables as variables
 
 
@@ -12,35 +9,43 @@ class FileProcesser(object):
     function: type = field(default_factory=lambda: variables.VariableManager)
     data: tuple = field(default_factory=tuple)
 
-    def iterate_files(self) -> list:
-        return [file_processor(self.function) for file_processor in self.data]
+    def iterate_files(self) -> "list|None":
+        for file_processor in self.data:
+            return file_processor(self.function)
 
 
 @dataclass
 class FileIterationProcessor(object):
-    processing_function: type = field(default_factory=lambda: variables.VariableManager)
-    files: Tuple[str, ...] = field(default_factory=lambda: ("", ""))
+    processing_function: type = field(
+        default_factory=lambda: variables.VariableManager
+    )
+    files: tuple[str, ...] = field(default_factory=lambda: ("", ""))
 
     def __post_init__(self) -> None:
         self.process_files()
 
-    @property
-    def dispatch_handler(self) -> DispatcherManager:
-        dispatch_handler = display_base_classes.DispatchHandler(
-            "file", self.processing_function
-        )
-        return dispatch_handler.create_dispatcher()
-
     def process_files(self) -> None:
-        [self.process_individual_file(file_name=file) for file in self.files]
+        for file_name in self.files:
+            IndividualFileProcessor(self.processing_function, file_name)
 
-    def process_individual_file(self, file_name: str) -> None:
+
+@dataclass
+class IndividualFileProcessor(object):
+    processing_function: type = field(
+        default_factory=lambda: variables.VariableManager
+    )
+    file_name: str = field(default="")
+
+    def __post_init__(self) -> None:
+        self.process_individual_file()
+
+    def process_individual_file(self) -> None:
         try:
             dispatcher = self.processing_function.get_dispatcher("file")
-            value = dispatcher.get_value(key=file_name)
-            processor = display_base_classes.OutputFormatter(
-                title=f"{file_name}_name", value=value.file_name
+            value = dispatcher.get_value(key=self.file_name)
+            processor = dbc.OutputFormatter(
+                title=f"{self.file_name}_name", value=value.file_name
             )
             processor.format_and_print()
         except Exception as error_msg:
-            print(f"?{display_base_classes.ErrorMessage('file', file_name, error_msg)}")
+            print(f"?{dbc.ErrorMessage('file', self.file_name, error_msg)}")
