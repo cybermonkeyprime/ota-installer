bfaf8e48451fc7b63b7c351895f5c306851e4e79
userbrians <brians018883@gmail.com>
2025-12-06 00:37:08 -0800

    Refactor FileNameVariableManager class in file_name_variable_manager.py: Removed docstrings from lines 11-43 for parts, validator, and parser methods to simplify code and improve readability.

--- src/components/file/variables/file_name_variable_manager.py
+++ src/components/file/variables/file_name_variable_manager.py
@@ -10,43 +10,21 @@ class FileNameVariableManager(object):
     """
     A class to manage file names, providing functionality to parse and validate
     file paths.
-
-    Attributes:
-        file_path (Path): The file path to manage.
     """
 
     path: Path = field(default_factory=Path)
 
     @property
     def parts(self) -> FileNameParserStructure:
-        """
-        Parses the file name from the file path.
-
-        Returns:
-            FileNameParser: An instance of FileNameParser with parsed
-                file name.
-        """
+        """Parses the file name from the file path."""
 
         return self.parser()
 
     def validator(self) -> Path:
-        """
-        Validates the file path.
-
-        Returns:
-            Optional[Path]: The validated file path or None if invalid.
-        """
-
         """Validates the file path."""
         file_path_validation = FilePathValidation(file_path=self.path)
         return file_path_validation.validator()
 
     def parser(self) -> FileNameParserStructure:
-        """
-        Parses the file name from the file path.
-
-        Returns:
-            FileNameParser: An instance of FileNameParser with parsed
-                file name.
-        """
+        """Parses the file name from the file path."""
         return FileNameParserStructure(self.path.stem)
