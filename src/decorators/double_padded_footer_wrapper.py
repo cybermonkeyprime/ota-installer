02113d3efff346fb16d2dfcecbc8297adcfe2d02
userbrians <brians018883@gmail.com>
2025-12-09 01:28:09 -0800

    Removed logger import on line 5 and commented out logger.info on line 27 in double_padded_footer_wrapper.py

--- src/decorators/double_padded_footer_wrapper.py
+++ src/decorators/double_padded_footer_wrapper.py
@@ -4,7 +4,7 @@ from dataclasses import dataclass, field
 from functools import wraps
 from typing import cast
 
-from src.logger import logger
+# from src.logger import logger
 from src.types.decorators import GenericDecorator
 
 
@@ -23,7 +23,7 @@ class DoublePaddedFooterWrapper(GenericDecorator):
         def wrapper(*args: P.args, **kwargs: P.kwargs) -> R:
             result = function(*args, **kwargs)
             self.string_output(self.beginning)
-            logger.info(self.message)
+            # logger.info(self.message)
             self.string_output(self.message)
             self.string_output(self.ending)
             return result
