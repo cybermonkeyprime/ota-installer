538fcf78675eca42fce0c2636b8cd09e9876889e
userbrians <brians018883@gmail.com>
2025-12-09 05:53:35 -0800

    Removed unused imports and console/table initialization from iteration_processor_creator.py. Lines 3-6, 30-36 were modified to clean up the code and improve efficiency.

--- src/display/variables/processors/iteration_processor_creator.py
+++ src/display/variables/processors/iteration_processor_creator.py
@@ -3,9 +3,6 @@ from dataclasses import dataclass, field
 from enum import Enum
 from typing import Self
 
-from rich.console import Console
-from rich.table import Table
-
 import src.dispatchers.mappings as dispatcher_mappings
 import src.display.variables.functions as functions
 import src.variables as variables
@@ -30,10 +27,6 @@ class IterationProcessorCreator(object):
         return self
 
     def process_items(self) -> None:
-        console = Console()
-        table = Table(title="", box=None)
-        table.add_column("Column 1", style="cyan", no_wrap=True)
-        table.add_column("Column 2", justify="right", style="magenta")
         for _ in self.iteration:
             try:
                 data_enum = Enum(
@@ -43,13 +36,8 @@ class IterationProcessorCreator(object):
                         "VALUE": "",
                     },
                 )
-                table.add_row(
-                    f"B{data_enum.TITLE.value.upper()}:",
-                    f"{data_enum.VALUE.value}",
-                )
-                # functions.parse_output(data_enum)
+                functions.parse_output(data_enum)
             except Exception as e:
                 logger.error(
                     f"[Error] Processing {self.title} {self.iteration} failed: {e}"
                 )
-        console.print(table)
