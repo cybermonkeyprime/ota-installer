1bbbc8ce4d38231b96a0f7c1e72939021fc8a7b3
userbrians <brians018883@gmail.com>
2025-12-09 01:48:40 -0800

    Updated functions: Changed set_log_file function on lines 4-6 to use file_name_bits instead of file_name_parser.

--- src/variables/functions.py
+++ src/variables/functions.py
@@ -4,9 +4,9 @@ from src.structures import FileNameParser
 from src.tasks import image_handler
 
 
-def set_log_file(file_name_parser: FileNameParser) -> str:
-    device = file_name_parser.device
-    version = file_name_parser.version
+def set_log_file(file_name_bits: FileNameParser) -> str:
+    device = file_name_bits.device
+    version = file_name_bits.version
     return f"/tmp/ota_variables_{device}_{version}.txt"
 
 
@@ -18,7 +18,7 @@ def get_image_path(device_name) -> Path:
     return image_handler(device_name)
 
 
-def set_variable_manager(path: Path) -> "VariableManager":
+def set_variable_manager(path: Path) -> "VariableManager":  # type: ignore[return-value]
     from src.variables import VariableManager
 
     return VariableManager(path)
