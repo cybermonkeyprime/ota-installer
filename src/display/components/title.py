6751cc1e373bea29df0608e3335c8616146dd0a2
userbrians <brians018883@gmail.com>
2025-12-10 04:46:50 -0800

    Updated src/display/components/title.py: replaced ColorizedFiglet decorator with StyledFigletPrinter on line 9.

--- src/display/components/title.py
+++ src/display/components/title.py
@@ -1,7 +1,7 @@
 from dataclasses import dataclass, field
 
-from src.decorators import ColorizedFiglet
 import src.display.templates as templates
+from src.decorators import StyledFigletPrinter
 
 
 @dataclass
@@ -9,21 +9,14 @@ class Title(templates.DisplayComponent):
     """
     A class representing a title component that can be displayed with special
         formatting.
-
-    Attributes:
-        title (str): The title text to be displayed. Defaults to
-            "ota-installer".
     """
 
     title: str = field(default="ota-installer")
 
-    @ColorizedFiglet(style="title", font="slant")
+    @StyledFigletPrinter(style="title", font="slant")
     def return_display(self) -> str:
         """
         Apply the ColorizedFiglet decorator to format the title.
-
-        Returns:
-            str: The formatted title string.
         """
         return f" {self.title}"
 
@@ -31,9 +24,6 @@ class Title(templates.DisplayComponent):
         """
         Retrieve the display string of the title, formatted by the
             ColorizedFiglet decorator.
-
-        Returns:
-            str: The formatted title string.
         """
         return self.return_display()
 
@@ -42,10 +32,6 @@ class Title(templates.DisplayComponent):
 class DisplayTitle(Title):
     """
     A class for creating a displayable title string.
-
-    Attributes:
-        title (str): The title text to be displayed. Defaults to an empty
-            string.
     """
 
     title: str = field(default_factory=str)
@@ -54,8 +40,5 @@ class DisplayTitle(Title):
         """
         Generate a string representation of the DisplayTitle instance, which
             includes the formatted title.
-
-        Returns:
-            str: The formatted title string.
         """
         return self.get_display()
