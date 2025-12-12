dfa4df53cd9a05006832698c0df3914d91466b11
userbrians <brians018883@gmail.com>
2025-12-12 08:09:48 -0800

    In src/types/directory/default_type_manager.py: Removed DefaultTypeDirectories class from lines 5 to 9. Updated to use BootImagePaths on lines 16 and 17.

--- src/types/directory/default_type_manager.py
+++ src/types/directory/default_type_manager.py
@@ -1,14 +1,10 @@
+# src/types/directory/default_type_manager.py
 from dataclasses import dataclass
-from enum import Enum
 from pathlib import Path
 
 import src.types.directory as directory
 from src.logger import logger
-
-
-class DefaultTypeDirectories(Enum):
-    STOCK = Path.home() / "Android" / "boot-images"
-    MAGISK = Path.home() / "sdcard" / "Download" / "magisk"
+from src.paths.constants import BootImagePaths
 
 
 @dataclass
@@ -20,8 +16,8 @@ class DefaultTypeManager(object):
         try:
             return directory.DefaultTypeDefinition(
                 self.parent_directory,
-                str(DefaultTypeDirectories.STOCK.value),
-                DefaultTypeDirectories.MAGISK.value,
+                str(BootImagePaths.STOCK.value),
+                BootImagePaths.MAGISK.value,
             )
         except Exception as err:
             logger.exception(f"{type(err).__name__}: {err}")
