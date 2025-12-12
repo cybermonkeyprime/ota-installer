3b26683617afa704b0bfd129130dc8c4a5f41738
userbrians <brians018883@gmail.com>
2025-12-12 08:09:40 -0800

    Refactored image_file.py: Replaced dataclass 'ImageFile' with namedtuple on lines 1-2, removed old class definition and methods on lines 4-17.

--- src/structures/image_file.py
+++ src/structures/image_file.py
@@ -1,17 +1,4 @@
-from dataclasses import dataclass, field
-from pathlib import Path
+# src/structures/image_file.py
+from collections import namedtuple
 
-
-@dataclass
-class ImageFile(object):
-    file_name: Path = field(default_factory=Path)
-    directory_path: Path = field(default_factory=Path)
-
-    def __post_init__(self) -> None:
-        self.file_path = self.file_name
-
-    def get_full_path(self) -> Path:
-        return Path(self.directory_path) / self.file_name
-
-    def __str__(self) -> str:
-        return str(self.get_full_path())
+ImageFile = namedtuple("ImageFile", ["file_path", "directory_path"])
