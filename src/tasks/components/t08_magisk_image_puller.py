8c4db37276114f303fd17a4a6355453fbf07342f
userbrians <brians018883@gmail.com>
2025-12-13 05:53:43 -0800

    Refactored source and destination paths in MagiskImagePuller class (lines 18-30) to use MagiskImagePaths constants.

--- src/tasks/components/t08_magisk_image_puller.py
+++ src/tasks/components/t08_magisk_image_puller.py
@@ -1,9 +1,9 @@
 # src/tasks/components/t08_magisk_image_puller.py
 from dataclasses import dataclass, field
-from pathlib import Path
 
 import src.variables as variables
 from src import decorators
+from src.paths.constants import MagiskImagePaths
 from src.tasks.task_operation_details import TaskOperationDetails
 
 from .base_task import BaseTask
@@ -18,20 +18,15 @@ class MagiskImagePuller(BaseTask):
     )
 
     def __post_init__(self) -> None:
-        magisk_path = self.instance.boot_image_paths.magisk
-        magisk_directory = self.instance.directory.magisk_image
-
-        magisk_local_path = Path(magisk_directory.local_path)
-        magisk_remote_path = Path(magisk_directory.remote_path)
-
-        source_path: Path = (
-            Path(magisk_remote_path) / self.instance.patched_image_name
+        source = (
+            MagiskImagePaths.REMOTE_PATH.value
+            / self.instance.patched_image_name
         )
-        destination_path = (
-            Path.home() / magisk_local_path / magisk_path.file_name
+        destination = (
+            MagiskImagePaths.LOCAL_PATH.value / self.instance.paths["magisk"]
         )
 
-        command_string = f'adb pull "{source_path}" "{destination_path}"'
+        command_string = f'adb pull "{source}" "{destination}"'
 
         super().__init__(
             enum_values=ENUM_VALUES,
