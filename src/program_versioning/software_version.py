e7b6aecb0cab4539d13f2874e82137b48c092ed6
userbrians <brians018883@gmail.com>
2025-12-14 01:08:26 -0800

    Updated PATCH_NUMBER from 13 to 14 in software_version.py on line 12.

--- src/program_versioning/software_version.py
+++ src/program_versioning/software_version.py
@@ -9,7 +9,7 @@ class SoftwareVersionConstants(Enum):
     TITLE = "OTA-Installer"
     MAJOR_NUMBER = 2025
     MINOR_NUMBER = 12
-    PATCH_NUMBER = 13
+    PATCH_NUMBER = 14
 
     def __str__(self) -> str:
         return str(self.value)
