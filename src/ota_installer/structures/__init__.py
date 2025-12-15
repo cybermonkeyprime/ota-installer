30c398170fc9e437580c023e387607a2ca60679a
userbrians <brians018883@gmail.com>
2025-12-15 04:02:31 -0800

    Removed import of directory from __init__.py on line 4.

--- src/ota_installer/structures/__init__.py
+++ src/ota_installer/structures/__init__.py
@@ -1,7 +1,6 @@
 from .android_image_file import *
 from .android_image_file_attributes import *
 from .boot_image import *
-from .directory import *
 from .file_name_parser import *
 from .image_file import *
 from .magisk import *
