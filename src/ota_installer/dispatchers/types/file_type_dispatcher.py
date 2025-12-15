f93d6380b0f3bd41db5f98f59fa46ab6bc380da9
userbrians <brians018883@gmail.com>
2025-12-15 09:26:11 -0800

    Updated import statement on line 4 to use relative import for DispatcherCreator.

--- src/ota_installer/dispatchers/types/file_type_dispatcher.py
+++ src/ota_installer/dispatchers/types/file_type_dispatcher.py
@@ -3,7 +3,7 @@ from dataclasses import dataclass, field
 from pathlib import Path
 from typing import Literal, TypeVar
 
-from src.ota_installer.dispatchers.templates import DispatcherCreator
+from ...dispatchers.templates import DispatcherCreator
 
 T = TypeVar("T")
 
