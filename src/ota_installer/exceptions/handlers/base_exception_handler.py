65893dbf11db4aa0792afd3256d1d43fbf1d651f
userbrians <brians018883@gmail.com>
2025-12-15 09:26:26 -0800

    Updated base_exception_handler.py: Changed import statement on line 4.

--- src/ota_installer/exceptions/handlers/base_exception_handler.py
+++ src/ota_installer/exceptions/handlers/base_exception_handler.py
@@ -2,7 +2,7 @@
 from collections.abc import Callable
 from dataclasses import dataclass, field
 
-from src.ota_installer.decorators import (
+from ...decorators import (
     Colorizer,
     IndentWrapper,
     OutputPrinter,
