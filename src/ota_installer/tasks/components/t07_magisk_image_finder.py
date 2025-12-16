5e6ef7671990b39746711e93b1cb819a6a73eaed
userbrians <brians018883@gmail.com>
2025-12-16 01:31:59 -0800

    Added task_plugin decorator for MagiskImageFinder on line 10 and imported TaskName and task_plugin on lines 3 and 4. Modified command_string on lines 15-17.

--- src/ota_installer/tasks/components/t07_magisk_image_finder.py
+++ src/ota_installer/tasks/components/t07_magisk_image_finder.py
@@ -3,20 +3,23 @@ from dataclasses import dataclass, field
 
 from ... import decorators
 from ...variables import VariableManager
+from ..mappings.constants import TaskName
+from ..plugin_registry import task_plugin
 from ..task_operation_details import TaskOperationDetails
 from .base_task import BaseTask
 
 ENUM_VALUES = TaskOperationDetails.FIND_MAGISK_IMAGE.value
 
 
+@task_plugin(TaskName.FIND_PATCHED_BOOT_IMAGE.under_case)
 @dataclass
 class MagiskImageFinder(BaseTask):
     instance: VariableManager = field(default_factory=VariableManager)
 
     def __post_init__(self) -> None:
-        remote_path = self.instance.directory.magisk_image.remote_path
         command_string = (
-            f'adb shell ls "{remote_path}" | grep magisk_patched | head -n1'
+            f'adb shell ls "{self.instance.directories["magisk"]["remote_path"]}"'
+            "| grep magisk_patched | head -n1"
         )
 
         super().__init__(
