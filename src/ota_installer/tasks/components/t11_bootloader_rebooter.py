425227f3a11341ce5b5d89adde30578906bd3a29
userbrians <brians018883@gmail.com>
2025-12-16 01:31:59 -0800

    Added task_plugin decorator for BootloaderRebooter on line 10 and imported TaskName and task_plugin on lines 3 and 4.

--- src/ota_installer/tasks/components/t11_bootloader_rebooter.py
+++ src/ota_installer/tasks/components/t11_bootloader_rebooter.py
@@ -3,12 +3,15 @@ from dataclasses import dataclass, field
 
 from ... import decorators
 from ...variables import VariableManager
+from ..mappings.constants import TaskName
+from ..plugin_registry import task_plugin
 from ..task_operation_details import TaskOperationDetails
 from .base_task import BaseTask
 
 ENUM_VALUES = TaskOperationDetails.REBOOT_TO_BOOTLOADER.value
 
 
+@task_plugin(TaskName.REBOOT_TO_BOOTLOADER.under_case)
 @dataclass
 class BootloaderRebooter(BaseTask):
     instance: VariableManager = field(default_factory=VariableManager)
