cc02d434f3818eda899a4886abe4a7f00a2a085b
userbrians <brians018883@gmail.com>
2025-12-17 08:09:15 -0800

    Changed ENUM_VALUES assignment from PAYLOAD_IMAGE_EXTRACTOR to EXTRACT_PAYLOAD_IMAGE on line 7.

--- src/ota_installer/tasks/components/t01_payload_image_extractor.py
+++ src/ota_installer/tasks/components/t01_payload_image_extractor.py
@@ -7,7 +7,7 @@ from ..plugin_registry import task_plugin
 from ..task_operation_details import TaskOperationDetails
 from .base_task import BaseTask
 
-ENUM_VALUES = TaskOperationDetails.PAYLOAD_IMAGE_EXTRACTOR.value
+ENUM_VALUES = TaskOperationDetails.EXTRACT_PAYLOAD_IMAGE.value
 
 
 @task_plugin(PreparationTasks.EXTRACT_PAYLOAD_IMAGE.value)
