4f4e3838ddea2a1a25e65dfe518352cfd72a0c25
userbrians <brians018883@gmail.com>
2025-12-17 07:15:01 -0800

    Added namedtuple TaskGroupData on line 2, and defined PreparationTasks and TaskMapping classes on lines 11-20 in task_group_type_mapping.py.

--- src/ota_installer/dispatchers/mappings/task_group_type_mapping.py
+++ src/ota_installer/dispatchers/mappings/task_group_type_mapping.py
@@ -1,4 +1,7 @@
+from collections import namedtuple
 from enum import Enum
+
+TaskGroupData = namedtuple("TaskGroupData", ["name", "task_enum"])
 
 
 class TaskGroupTypeMapping(Enum):
@@ -8,3 +11,13 @@ class TaskGroupTypeMapping(Enum):
 
     def _value(self, obj: type) -> object:
         return getattr(obj, self.value)
+
+
+class PreparationTasks(Enum):
+    pass
+
+
+class TaskMapping(Enum):
+    PREPARATION = TaskGroupData("preparation", PreparationTasks)
+    MIGRATION = "migration"
+    APPLICATION = "application"
