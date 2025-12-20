c52f8403177415993fb87d63de22fb0b3c9552fc
userbrians <brians018883@gmail.com>
2025-12-20 04:37:53 -0800

    Updated import statements for TaskOperationDetails and image_handler in t12_magisk_image_booter.py on lines 4 and 5

--- src/ota_installer/tasks/components/t12_magisk_image_booter.py
+++ src/ota_installer/tasks/components/t12_magisk_image_booter.py
@@ -4,9 +4,9 @@ from dataclasses import dataclass, field
 from ... import decorators
 from ...variables import VariableManager
 from ..constants.application_task_constants import ApplicationTaskConstants
+from ..operations.task_operation_details import TaskOperationDetails
+from ..operations.task_operation_processor import image_handler
 from ..plugin_registry import task_plugin
-from ..task_operation_details import TaskOperationDetails
-from ..task_operation_processor import image_handler
 from .base_task import BaseTask
 
 ENUM_VALUES = TaskOperationDetails.BOOT_TO_MAGISK_IMAGE.value
