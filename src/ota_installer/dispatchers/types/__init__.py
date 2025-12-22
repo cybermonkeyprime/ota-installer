ce412ad08a233887016c8a79e004ae8262210163
userbrians <brians018883@gmail.com>
2025-12-21 23:14:01 -0800

    Modified __init__.py: removed import of image_type_dispatcher on line 4.

--- src/ota_installer/dispatchers/types/__init__.py
+++ src/ota_installer/dispatchers/types/__init__.py
@@ -1,5 +1,4 @@
 from .directory_type_dispatcher import *
 from .file_type_dispatcher import *
-from .image_type_dispatcher import *
 from .task_group_type_dispatcher import *
 from .variable_type_dispatcher import *
