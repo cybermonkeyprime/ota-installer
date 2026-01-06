f5133d1cef4478c3cf3af119d234d1c48bf96753
userbrians <brians018883@gmail.com>
2026-01-06 04:21:17 -0800

    Removed the Separator class and its associated methods from src/ota_installer/styles/separator.py. Lines 2-5 and 11-24 were deleted, simplifying the file.

--- src/ota_installer/styles/separator.py
+++ src/ota_installer/styles/separator.py
@@ -1,5 +1,4 @@
 # src/ota_installer/styles/separator.py
-from dataclasses import dataclass
 from enum import Enum
 
 from .indentation import indentation
@@ -9,24 +8,6 @@ class SeparatorConstants(Enum):
     CHAR = "-"
     SPACING = 4
     INTERVAL = 10
-
-
-@dataclass
-class Separator(object):
-    increment: int = 1
-    char: str = "-"
-
-    @property
-    def data(self) -> list:
-        return [enum_member.value for enum_member in SeparatorConstants]
-
-    def __str__(self) -> str:
-        sc = SeparatorConstants
-        return indentation(
-            char=sc.CHAR.value,
-            spaces=sc.SPACING.value,
-            interval=sc.INTERVAL.value,
-        )
 
 
 def separator(increment: int = 1, char: str = "-") -> str:
