0a42ae243119d983df2d94262afa521217463ff6
userbrians <brians018883@gmail.com>
2026-01-08 04:31:00 -0800

    Removed src/ota_installer/display/objects/containers/display_object_container.py: deleted the entire file which contained the DisplayObjectContainer namedtuple definition.

--- src/ota_installer/display/objects/containers/display_object_container.py
+++
@@ -1,7 +0,0 @@
-# src/ota_installer/display/objects/containers/display_object_container.py
-from collections import namedtuple
-
-DisplayObjectContainer = namedtuple(
-    "DisplayObjectContainer",
-    ["object_name", "function"],
-)
