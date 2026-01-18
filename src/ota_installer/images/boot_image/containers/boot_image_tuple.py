cb9b16cfa0dc4a51c4e7355a1c372e5cad33cea6
Brian Sanford <brians018883@gmail.com>
2026-01-18 02:21:41 -0800

    Refactor BootImageContainer to use NamedTuple and Path for type safety.

--- src/ota_installer/images/boot_image/containers/boot_image_tuple.py
+++ src/ota_installer/images/boot_image/containers/boot_image_tuple.py
@@ -1,6 +1,13 @@
 # src/ota_installer/images/boot_image/containers/boot_image_tuple.py
-from collections import namedtuple
+from pathlib import Path
+from typing import NamedTuple
 
-BootImageTuple = namedtuple("BootImageContainer", ["stock", "magisk"])
 
-# data = BootImageContainer(*boot_image_paths)
+class BootImageTuple(NamedTuple):
+    """Represents a boot image with stock and magisk paths."""
+
+    stock: Path
+    magisk: Path
+
+
+# Signed off by Brian Sanford on 20260118
