5e43aaa09cc0028dffb821f9fdc8aeab934d66eb
Brian Sanford <brians018883@gmail.com>
2026-01-18 22:30:36 -0800

    Add sign-off by Brian Sanford on 20260118 in dispatcher_plugin_loader.py

--- src/ota_installer/dispatchers/dispatcher_plugin_loader.py
+++ src/ota_installer/dispatchers/dispatcher_plugin_loader.py
@@ -14,3 +14,4 @@ from ..task_groups.dispatchers.task_group_type_dispatcher import (
 from ..variables.dispatchers.variable_type_dispatcher import (
     VariableTypeDispatcher,
 )
+# Signed off by Brian Sanford on 20260118
