66c6f79e3ce1bae0fb8b070d8388c521985947d5
Brian Sanford <brians018883@gmail.com>
2026-01-18 22:20:19 -0800

    Add docstrings to DisplayComponent interface methods in display_template.py

--- src/ota_installer/display/templates/display_template.py
+++ src/ota_installer/display/templates/display_template.py
@@ -3,5 +3,15 @@ from typing import Protocol
 
 
 class DisplayComponent(Protocol):
+    """Interface for display components that provide a display string.
+
+    This interface defines a method to retrieve the display string
+    for a component.
+    """
+
     def get_display(self) -> str:
+        """Return the display string for the component."""
         raise NotImplementedError()
+
+
+# Signed off by Brian Sanford on 20260118
