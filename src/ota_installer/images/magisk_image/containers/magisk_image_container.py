1d5e3693fc8c8225a6352f3aa5cc36b2f2969283
Brian Sanford <brians018883@gmail.com>
2026-01-19 23:17:21 -0800

    Enhance MagiskImageContainer with type hints and docstrings for clarity.

--- src/ota_installer/images/magisk_image/containers/magisk_image_container.py
+++ src/ota_installer/images/magisk_image/containers/magisk_image_container.py
@@ -1,5 +1,6 @@
 # src/ota_installer/images/magisk_image/containers/magisk.py
 from dataclasses import dataclass
+from pathlib import Path
 
 from ..constants.magisk_image_paths import MagiskImagePaths
 from .magisk_image_tuple import MagiskImageTuple
@@ -7,18 +8,29 @@ from .magisk_image_tuple import MagiskImageTuple
 
 @dataclass
 class MagiskImageContainer(object):
+    """Container for managing Magisk image paths."""
+
     @property
     def path_data(self) -> list:
+        """
+        Returns a list of path values from the Magisk image paths enumeration.
+        """
         return [enum_member.value for enum_member in MagiskImagePaths]
 
     @property
-    def path_container(self):
+    def path_container(self) -> MagiskImageTuple:
+        """Returns a MagiskImageTuple containing local and remote paths."""
         return MagiskImageTuple(*self.path_data)
 
     @property
-    def local_path(self):
+    def local_path(self) -> Path:
+        """Returns the local path from the path container."""
         return self.path_container.local_path
 
     @property
-    def remote_path(self):
+    def remote_path(self) -> Path:
+        """Returns the remote path from the path container."""
         return self.path_container.remote_path
+
+
+# Signed off by Brian Sanford on 20260119
