15871ef60b6c495239e6c1eee3af9ddbf753bfef
Brian Sanford <brians018883@gmail.com>
2026-01-20 03:10:37 -0800

    Add docstring to DirectoryNames class in directory_names.py

--- src/ota_installer/variables/constants/directory_names.py
+++ src/ota_installer/variables/constants/directory_names.py
@@ -6,4 +6,9 @@ from .directory_paths import DirectoryPaths
 
 @dataclass(frozen=True, slots=True)
 class DirectoryNames(object):
+    """Container for directory names used in the OTA installer."""
+
     magisk: DirectoryPaths
+
+
+# Signed off by Brian Sanford on 20260120
