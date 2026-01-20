2f5ff4c203ef69e7676edd72418a39c2f449d971
Brian Sanford <brians018883@gmail.com>
2026-01-19 23:55:10 -0800

    Refactor VariableItem to use dataclass
     • Replaced namedtuple with a frozen dataclass for VariableItem.
     • Added type annotations for title and value attributes.

--- src/ota_installer/display/variables/containers/variable_item.py
+++ src/ota_installer/display/variables/containers/variable_item.py
@@ -1,4 +1,13 @@
 # src/ota_installer/display/variables/classes/variable_item.py
-from collections import namedtuple
+from dataclasses import dataclass
 
-VariableItem = namedtuple("VariableItem", ["title", "value"])
+
+@dataclass(frozen=True, slots=True)
+class VariableItem(object):
+    """Represents a variable item with a title and a value."""
+
+    title: str
+    value: str | None
+
+
+# Signed off by Brian Sanford on 20260119
