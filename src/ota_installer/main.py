4257fa414a975f4ec2e797886f2c458810798a8c
Brian Sanford <brians018883@gmail.com>
2026-01-20 02:09:37 -0800

    Add docstring to main function and preserve plugin loader comment

--- src/ota_installer/main.py
+++ src/ota_installer/main.py
@@ -1,13 +1,21 @@
 #!/usr/bin/env python3
 
 from .command_line_interface import cli
+
+# DO NOT REMOVE, PLUGIN LOADERS!!!
 from .dispatchers import dispatcher_plugin_loader
 from .tasks import plugin_loader
 
 
 def main() -> None:
+    """Entry point for the application.
+
+    This function initializes the command line interface.
+    """
     cli()
 
 
 if __name__ == "__main__":
     main()
+
+# Signed off by Brian Sanford on 20260120
