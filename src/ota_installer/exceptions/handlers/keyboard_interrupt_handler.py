dfa0621a96e0cab96a3fd988229a69193d261dc1
Brian Sanford <brians018883@gmail.com>
2026-01-20 00:28:33 -0800

    Add docstring and default message to KeyboardInterruptHandler class
    Signed-off-by: Brian Sanford your_email@example.com

--- src/ota_installer/exceptions/handlers/keyboard_interrupt_handler.py
+++ src/ota_installer/exceptions/handlers/keyboard_interrupt_handler.py
@@ -8,4 +8,9 @@ from .exception_handler_factory import exception_handler_factory
 @exception_handler_factory(KeyboardInterrupt)
 @dataclass
 class KeyboardInterruptHandler(BaseExceptionHandler):
+    """Handles KeyboardInterrupt exceptions."""
+
     default_message: str = "Keyboard interrupt detected, quitting!"
+
+
+# Signed off by Brian Sanford on 20260120
