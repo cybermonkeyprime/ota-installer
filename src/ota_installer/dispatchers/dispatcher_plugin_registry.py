ea991653f4451c1ccabdeebe60c9b81bc6e21679
Brian Sanford <brians018883@gmail.com>
2026-02-02 01:23:54 -0800

    Refactor dispatcher_plugins type annotation to use dict[str, Callable] for clarity.

--- src/ota_installer/dispatchers/dispatcher_plugin_registry.py
+++ src/ota_installer/dispatchers/dispatcher_plugin_registry.py
@@ -1,7 +1,7 @@
 # src/ota_installer/tasks/plugin_registry.py
 from collections.abc import Callable
 
-DISPATCHER_PLUGINS = {}
+DISPATCHER_PLUGINS: dict[str, Callable] = {}
 
 
 def dispatcher_plugin(name) -> Callable:
@@ -14,3 +14,4 @@ def dispatcher_plugin(name) -> Callable:
     return decorator
 
 
+# Signed off by Brian Sanford on 20260202
