855216655588b75a246c74e86c87bc70bc60e31e
Brian Sanford <brians018883@gmail.com>
2026-02-02 02:14:14 -0800

    Refactor display_subtitle docstring to be more concise.

--- src/ota_installer/display/components/subtitle.py
+++ src/ota_installer/display/components/subtitle.py
@@ -6,11 +6,8 @@ from ...program_versioning.software_version import get_display
 
 @Colorizer(style="version")
 def display_subtitle() -> str:
-    """Generate a subtitle displaying the current software version.
-
-    Returns:
-        str: The string representation of the current software version.
-    """
+    """Generate a subtitle displaying the current software version."""
     return get_display()
 
 
+# Signed off by Brian Sanford on 20260202
