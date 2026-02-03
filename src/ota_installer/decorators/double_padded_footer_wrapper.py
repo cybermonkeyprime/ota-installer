e23c90b69405e9cae326e536bf4a6c0bd81bfa07
Brian Sanford <brians018883@gmail.com>
2026-02-03 05:12:02 -0800

    Refactor DoublePaddedFooterWrapper to use dependency injection for Colorizer, IndentWrapper, and OutputPrinter; simplify call method signature.

--- src/ota_installer/decorators/double_padded_footer_wrapper.py
+++ src/ota_installer/decorators/double_padded_footer_wrapper.py
@@ -2,7 +2,8 @@
 from collections.abc import Callable
 from dataclasses import dataclass, field
 from functools import wraps
-from typing import cast
+
+from dependency_injector import containers, providers
 
 from ..log_setup import logger
 from .protocols.decorator_protocols import GenericDecorator
@@ -18,15 +19,20 @@ class DoublePaddedFooterWrapper(GenericDecorator):
     message: str = field(default="Finished!")
     ending: str = field(default="")
 
-    from . import Colorizer
-    from .indent_wrapper import IndentWrapper
-    from .output_printer import OutputPrinter
+    class Container(containers.DeclarativeContainer):
+        from . import Colorizer
+        from .indent_wrapper import IndentWrapper
+        from .output_printer import OutputPrinter
 
-    def __call__[R, **P](self, function: Callable[P, R]) -> Callable[P, R]:
+        colorizer = providers.Factory(Colorizer, style="variable")
+        indent_wrapper = providers.Factory(IndentWrapper, interval=1)
+        output_printer = providers.Factory(OutputPrinter, use_color=False)
+
+    def __call__(self, function: Callable) -> Callable:
         """Wraps the function to add a double padded footer."""
 
         @wraps(function)
-        def wrapper(*args: P.args, **kwargs: P.kwargs) -> R:
+        def wrapper(*args, **kwargs) -> object:
             result = function(*args, **kwargs)
             self._output_footer(self.beginning)
             logger.debug(self.message)
@@ -34,11 +40,11 @@ class DoublePaddedFooterWrapper(GenericDecorator):
             self._output_footer(self.ending)
             return result
 
-        return cast(Callable[P, R], wrapper)
+        return wrapper
 
-    @OutputPrinter(use_color=False)
-    @Colorizer(style="variable")
-    @IndentWrapper(interval=1)
+    @Container.output_printer()
+    @Container.colorizer()
+    @Container.indent_wrapper()
     def _output_footer(self, message: str) -> str:
         """Outputs the footer messages."""
         return f"{message}"
