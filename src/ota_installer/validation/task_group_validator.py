a82602f0c2965cb8faee8a6dd8bdc447e48d99ee
Brian Sanford <brians018883@gmail.com>
2026-02-13 15:22:30 -0800

    Refactor validate_task_group function to simplify error handling and remove console output for invalid task groups.

--- src/ota_installer/validation/task_group_validator.py
+++ src/ota_installer/validation/task_group_validator.py
@@ -1,21 +1,7 @@
 # validation/task_group_validator.py
-from rich.console import console
-
 from ..task_groups.constants.task_group_names import TaskGroupNames
 
 
 def validate_task_group(value: str) -> TaskGroupNames:
     """Validate the provided task group name."""
-    try:
-        return TaskGroupNames[value.upper()]
-    except KeyError:
-        validate_task_groups = ", ".join(
-            [task_group.name for task_group in TaskGroupNames]
-        )
-        console.print(
-            f"[red]Error:[/red] Invalid task group '{value}'. "
-            f"Valid options are: [cyan]{validate_task_groups}[/cyan]"
-        )
-        raise SystemExit(1) from None
-
-
+    return TaskGroupNames[value.upper()] or None
