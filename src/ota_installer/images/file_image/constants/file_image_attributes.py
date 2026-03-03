e4095e37fbbb43916cef869f246da6800d426745
Brian Sanford <brians018883@gmail.com>
2026-03-03 06:38:43 -0800

    Refactor FileImageAttributes class to add methods for setting device and version attributes, and improve file name generation.

--- src/ota_installer/images/file_image/constants/file_image_attributes.py
+++ src/ota_installer/images/file_image/constants/file_image_attributes.py
@@ -25,21 +25,26 @@ class FileImageAttributes(Enum):
     @property
     def file_name(self) -> str:
         """Generates the formatted file name based on attributes."""
+
         return f"{self.device}-{self.title}-{self.version}.{self.extension}"
 
     def set_device(self, device: str) -> Self:
         """Sets the device attribute and returns the enum instance."""
+
         self.device = str(device)
         return self
 
     def set_version(self, version: str) -> Self:
         """Sets the version attribute and returns the enum instance."""
+
         self.version = str(version)
         return self
 
     def set_file_path(self) -> Path:
         """Constructs the full file path for the image file."""
+
         boot_image_path = BootImagePaths[self.name].value
         return Path(boot_image_path) / self.file_name
 
 
+# Signed off by Brian Sanford on 20260303
