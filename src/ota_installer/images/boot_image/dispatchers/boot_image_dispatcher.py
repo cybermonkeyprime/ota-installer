9dc5db83dda3e1e63d13ca1129836151731128b1
Brian Sanford <brians018883@gmail.com>
2026-03-03 06:16:09 -0800

    Refactor ImageTypes to ImageType using StrEnum; add allowed_types method for lowercase keys. Update ImageTypeDispatcher to utilize new enum structure and improve type hints.

--- src/ota_installer/images/boot_image/dispatchers/boot_image_dispatcher.py
+++ src/ota_installer/images/boot_image/dispatchers/boot_image_dispatcher.py
@@ -1,14 +1,19 @@
 # src/ota_installer/images/boot_image/dispatchers/boot_image_dispatcher.py
 from dataclasses import dataclass, field
-from enum import Enum
+from enum import StrEnum
 
 from ....dispatchers.templates.dispatcher_template import DispatcherTemplate
 
 
-class ImageTypes(Enum):
+class ImageType(StrEnum):
     TOKAY = "init_boot"
     SHIBA = "init_boot"
     DEFAULT = "init_boot"
+
+    @classmethod
+    def allowed_types(cls) -> tuple:
+        """Returns a tuple of allowed image type keys in lowercase."""
+        return tuple(enum.name.lower() for enum in cls)
 
 
 @dataclass
@@ -16,11 +21,11 @@ class ImageTypeDispatcher(DispatcherTemplate):
     obj: type = field(default_factory=lambda: type)
 
     @property
-    def allowed_keys(self) -> tuple:
+    def allowed_keys(self) -> tuple[str, ...]:
         """Returns a tuple of allowed image type keys in lowercase."""
-        return tuple(enum.name.lower() for enum in ImageTypes)
+        return ImageType.allowed_types()
 
-    def get_key(self, key: str) -> object:
+    def get_key(self, key: str) -> str:
         """Normalizes the provided key and returns the corresponding image
         type value.
         """
@@ -30,7 +35,7 @@ class ImageTypeDispatcher(DispatcherTemplate):
             if normalized_key not in self.allowed_keys
             else normalized_key
         )
-        key_object = ImageTypes[evaluated_key]
-        return key_object.value
+        return ImageType[evaluated_key].value
 
 
+# Signed off by Brian Sanford on 20260303
