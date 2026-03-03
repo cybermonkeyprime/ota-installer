fe4944ae23564e4d46767aded19ec89371c294e0
Brian Sanford <brians018883@gmail.com>
2026-03-02 20:21:56 -0800

    Refactor ConfirmationPrompt usage in TaskDefinitions class to use partial function for improved readability.

--- src/ota_installer/tasks/definitions/task_definitions.py
+++ src/ota_installer/tasks/definitions/task_definitions.py
@@ -1,5 +1,6 @@
 # src/ota_installer/tasks/definitions/task_definitions.py
 from dataclasses import dataclass
+from functools import partial
 
 from ... import decorators
 from ...task_groups.constants.application_task import (
@@ -12,32 +13,27 @@ from ...task_groups.constants.preparation_task import (
     PreparationTask,
 )
 
+task_prompt = partial(decorators.ConfirmationPrompt, char=" ")
+
 
 @dataclass
 class TaskDefinitions(object):
     """Handles the definitions of various task categories."""
 
     @decorators.PaddedFooterWrapper()
-    @decorators.ConfirmationPrompt(
-        comment="perform the Preparation Tasks",
-        char=" ",
-    )
+    @task_prompt(comment="perform the Preparation Tasks")
     def preparation(self) -> tuple:
         """Returns the preparation task definitions."""
         return PreparationTask.get_member_names()
 
     @decorators.PaddedFooterWrapper()
-    @decorators.ConfirmationPrompt(
-        comment="perform the Migration Tasks", char=" "
-    )
+    @task_prompt(comment="perform the Migration Tasks")
     def migration(self) -> tuple:
         """Returns the migration task definitions."""
         return MigrationTask.get_member_names()
 
     @decorators.PaddedFooterWrapper()
-    @decorators.ConfirmationPrompt(
-        comment="perform the Application Tasks", char=" "
-    )
+    @task_prompt(comment="perform the Application Tasks")
     def application(self) -> tuple:
         """Returns the application task definitions."""
         return ApplicationTask.get_member_names()
