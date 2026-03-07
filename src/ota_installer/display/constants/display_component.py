0fb871a0f4cdac8b89460a293759917239dabf5a
Brian Sanford <brians018883@gmail.com>
2026-03-07 00:29:27 -0800

    Refactor DisplayComponent docstring for clarity and conciseness.

--- src/ota_installer/display/constants/display_component.py
+++ src/ota_installer/display/constants/display_component.py
@@ -3,12 +3,11 @@ from enum import StrEnum, auto
 
 
 class DisplayComponent(StrEnum):
-    """
-    Classifies different types of components that can be used in a display.
-    """
+    """Enumerates the various components used in a display."""
 
     TITLE = auto()
     SEPARATOR = auto()
     SUBTITLE = auto()
 
 
+# Signed off by Brian Sanford on 20260307
