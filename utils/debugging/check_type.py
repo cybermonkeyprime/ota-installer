88706030782eec2faa91fe69256ee4690a437059
Brian Sanford <brians018883@gmail.com>
2026-03-10 22:02:08 -0700

    Refactor TypeChecker to use dataclass; improve type information display logic.

--- utils/debugging/check_type.py
+++ utils/debugging/check_type.py
@@ -1,22 +1,28 @@
-from collections import namedtuple
+from dataclasses import dataclass
 
 from ...src.ota_installer.log_setup import logger
 
-TypeChecker = namedtuple("TypeChecker", ["name", "object_to_check"])
+
+@dataclass(frozen=True, slots=True)
+class TypeChecker(object):
+    """Represents an object type checker."""
+
+    name: str
+    object_to_check: object
 
 
 def display_type_information(type_checker: TypeChecker) -> None:
-    try:
-        object_type: type = type(type_checker.object_to_check)
-        title = type_checker.name.title()
-        _object = type_checker.object_to_check
-        object_name = object_type.__name__
+    object_type: type = type(type_checker.object_to_check)
+    title = type_checker.name.title()
+    _object = type_checker.object_to_check
+    object_name = object_type.__name__
+
+    if _object is not None:
         print(f'{title}: "{_object}" is a {object_name}')
-    except Exception as error:
-        logger.error(f"{type_checker.name.title()}: {error}")
+    else:
+        logger.error(f"{title}: Object is None")
 
 
 if __name__ == "__main__":
     type_checker = TypeChecker(name="example", object_to_check=123)
-    print(f"{display_type_information(type_checker)}")
-
+    display_type_information(type_checker)
