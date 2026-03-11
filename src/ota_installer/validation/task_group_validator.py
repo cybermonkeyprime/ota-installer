dc396689eac71cec23f6afe85900eedf3b5ac8ab
Brian Sanford <brians018883@gmail.com>
2026-03-10 21:52:59 -0700

    Remove task group validator implementation from the OTA installer validation module.

--- src/ota_installer/validation/task_group_validator.py
+++
@@ -1,7 +0,0 @@
-# validation/task_group_validator.py
-from ..task_groups.constants.task_group_names import TaskGroupNames
-
-
-def validate_task_group(value: str) -> bool:
-    """Validate the provided task group name."""
-    return TaskGroupNames[value.upper()].validation
