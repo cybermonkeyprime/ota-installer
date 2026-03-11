7a25b6626153cd6212f566842a3eb3381333e61e
Brian Sanford <brians018883@gmail.com>
2026-03-10 22:54:18 -0700

    Refactor ObjectInspector to use dependency injection; simplify inspect_object function and initialize object_name in constructor.

--- utils/debugging/object_inspector.py
+++ utils/debugging/object_inspector.py
@@ -2,28 +2,14 @@
 import inspect
 from typing import Self
 
-
-def inspect_object(object_name: inspect._IntrospectableCallable):
-    (
-        ObjectInspector()
-        .set_object_name(object_name)
-        .show_intro()
-        .show_type()
-        .show_source_code()
-        .show_signature()
-        .show_doc_strings()
-    )
+from dependency_injector import containers, providers
 
 
 class ObjectInspector(object):
     """An object inspector that shows a report of an object"""
 
-    def set_object_name(
-        self, object_name: inspect._IntrospectableCallable
-    ) -> Self:
-        """Set object name"""
+    def __init__(self, object_name: inspect._IntrospectableCallable) -> None:
         self.object_name = object_name
-        return self
 
     def show_intro(self) -> Self:
         """Show introduction"""
@@ -51,6 +37,23 @@ class ObjectInspector(object):
         return self
 
 
+def inspect_object(object_name: inspect._IntrospectableCallable):
+    (
+        ObjectInspectorContainer.object_inspector(object_name)
+        .show_intro()
+        .show_type()
+        .show_source_code()
+        .show_signature()
+        .show_doc_strings()
+    )
+
+
+class ObjectInspectorContainer(containers.DeclarativeContainer):
+    """Dependency injection container for ObjectInspector."""
+
+    object_inspector = providers.Factory(ObjectInspector)
+
+
 def main():
     """Main function to execute the object inspection."""
     inspect_object(ObjectInspector)
@@ -58,3 +61,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+# Signed off by Brian Sanford on 20260310
