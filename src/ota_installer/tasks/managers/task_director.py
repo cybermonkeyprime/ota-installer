2398789949a988895eb9e6642cf07a4f83ca6f05
Brian Sanford <brians018883@gmail.com>
2026-03-18 03:51:45 -0700

    Refactor task director to add _is_executable function for checking task validity.

--- src/ota_installer/tasks/managers/task_director.py
+++ src/ota_installer/tasks/managers/task_director.py
@@ -11,10 +11,13 @@ def task_director(instance: VariableManager, task_name: Callable) -> None:
     logger.debug(f"Initiating task: {task_name}")
     task = task_name(instance=instance)
 
-    if not hasattr(task, "perform_task"):
+    if not _is_executable(task):
         logger.error(f"Task {task_name!r} is missing perform_task() method.")
         raise ValueError(f"Task {task_name!r} is not executable.")
 
     task.perform_task()
 
 
+def _is_executable(task: object) -> bool:
+    """Checks if the task has a perform_task method."""
+    return hasattr(task, "perform_task")
