cd63e9daa7a88d654d9309736230a909f2b84480
Brian Sanford <brians018883@gmail.com>
2026-04-28 22:18:21 -0700

    Remove software_version.py file and its associated DisplayType class.

--- src/ota_installer/program_versioning/software_version.py
+++
@@ -1,9 +0,0 @@
-# src/ota_installer/program_versioning/software_version.py
-from enum import Enum
-
-from .constants.software_constants import SoftwareType
-
-
-class DisplayType(Enum):
-    VERBOSE = SoftwareType.render_display()
-    CONCISE = SoftwareType.render_text()
