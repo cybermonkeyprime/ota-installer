7eaed5ea72bee3437a0c81ba8d6b79275485d839
Brian Sanford <brians018883@gmail.com>
2026-05-09 21:40:44 -0700

    Refactor import statements in directory_handler.py for improved organization and clarity.

--- src/ota_installer/directory_handler.py
+++ src/ota_installer/directory_handler.py
@@ -4,9 +4,9 @@ from dataclasses import dataclass, field
 from enum import StrEnum, auto
 from pathlib import Path
 
-from .dispatchers.constants.dispatcher_type import DispatcherType
-from .dispatchers.dispatcher_plugin_registry import dispatcher_plugin
-from .dispatchers.templates.dispatcher_template import DispatcherTemplate
+from .dispatchers.dispatcher_template import DispatcherTemplate
+from .dispatchers.dispatcher_type import DispatcherType
+from .dispatchers.plugins.dispatcher_plugin_registry import dispatcher_plugin
 from .images.boot_image_handler import (
     BootImageContainer,
     BootImagePaths,
