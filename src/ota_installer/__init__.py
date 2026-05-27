f4e1bc58bebb92e4c1b4bd496136a4a8cf6470eb
Brian Sanford <brians018883@gmail.com>
2026-05-27 03:05:19 -0700

    Update pyproject.toml to adjust Ruff configuration and modify version string formatting in init.py.

--- src/ota_installer/__init__.py
+++ src/ota_installer/__init__.py
@@ -1,4 +1,8 @@
 from .handler.version_handler import SoftwareVersion
 
 version = SoftwareVersion
-__version__ = f"{version.MAJOR_NUMBER.value}.{version.MINOR_NUMBER.value}.{version.PATCH_NUMBER.value}"
+__version__ = (
+    f"{version.MAJOR_NUMBER.value}."
+    f"{version.MINOR_NUMBER.value}."
+    f"{version.PATCH_NUMBER.value}"
+)
