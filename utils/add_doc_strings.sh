a97d447748fad096b1d306249092f4d378553481
Brian Sanford <brians018883@gmail.com>
2026-05-27 02:47:00 -0700

    Remove unused Python scripts and shell scripts for docstring generation, error fixing, type checking, object inspection, and log viewing.

--- utils/add_doc_strings.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env zsh
-file_name="$1"
-
-request="create google style docstrings for each class/method and function in the following python script"
-
-sgpt "$request: $(batcat -p --color=never $file_name)"
