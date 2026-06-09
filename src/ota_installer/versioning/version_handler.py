dc04c38c19a2a1cf6d7a42907be4e38e4850f094
Brian Sanford <brians018883@gmail.com>
2026-06-09 01:40:13 -0700

    chore(version): update minor and patch numbers to 6 and 8

--- src/ota_installer/versioning/version_handler.py
+++ src/ota_installer/versioning/version_handler.py
@@ -8,8 +8,8 @@ class SoftwareVersion(Enum):
 
     TITLE = "OTA-Installer"
     MAJOR_NUMBER = 2026
-    MINOR_NUMBER = 5
-    PATCH_NUMBER = 22
+    MINOR_NUMBER = 6
+    PATCH_NUMBER = 8
 
     @classmethod
     def display(cls) -> str:
@@ -35,16 +35,6 @@ class SoftwareInfo:
     major_number: int
     minor_number: int
     patch_number: int
-
-
-def _create_software_info() -> SoftwareInfo:
-    """Creates a SoftwareContainer instance with versioning information."""
-    return SoftwareInfo(
-        title=SoftwareVersion.TITLE.value,
-        major_number=SoftwareVersion.MAJOR_NUMBER.value,
-        minor_number=SoftwareVersion.MINOR_NUMBER.value,
-        patch_number=SoftwareVersion.PATCH_NUMBER.value,
-    )
 
 
 # Signed off by Brian Sanford on 20260523
