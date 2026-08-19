7634bd5547d11fd5124239aa21c1e66e73a89fd1
Brian Sanford <brians018883@gmail.com>
2026-08-19 04:18:36 -0700

    Add to_context method to VariableDirector for variable serialization and debugging output.

--- src/ota_installer/variable/variable_director.py
+++ src/ota_installer/variable/variable_director.py
@@ -1,5 +1,5 @@
 # src/ota_installer/variables/variable_director.py
-from dataclasses import dataclass, field
+from dataclasses import asdict, dataclass, field
 from pathlib import Path
 from typing import Self
 
@@ -66,6 +66,22 @@ class VariableDirector:
     @property
     def file_parts(self) -> Path:
         return self.variables.file_parts
+
+    def to_context(self) -> tuple:
+        from pprint import pprint
+
+        variable_api = tuple(
+            {
+                "files_paths": asdict(self.file_paths),
+                "directory_paths": {
+                    "parent": self.ota_parent_directory,
+                    "boot_paths": asdict(self.boot_directories),
+                }
+                | asdict(self.directories),
+            }
+        )
+        pprint(variable_api)
+        return variable_api
 
     def get_dispatcher(self, process_type) -> object:
         """Retrieves the dispatcher for the given process type."""
