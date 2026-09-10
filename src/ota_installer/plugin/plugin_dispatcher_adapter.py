55ff3c4dc74d5568117ffaf4e9eda36bb0f1ba7d
Brian Sanford <brians018883@gmail.com>
2026-09-10 02:02:11 -0700

    Refactor logging in PluginDispatcherAdapter to use log_status instead of logger.

--- src/ota_installer/plugin/plugin_dispatcher_adapter.py
+++ src/ota_installer/plugin/plugin_dispatcher_adapter.py
@@ -2,7 +2,7 @@
 from dataclasses import dataclass, field
 
 from ..dispatcher.dispatcher_type import DispatcherType
-from ..log_setup import logger
+from ..log_setup import log_status
 from .plugin_registry import Plugin
 
 
@@ -25,11 +25,13 @@ class PluginDispatcherAdapter:
 
     def load(self) -> object:
         """Load the dispatcher based on the specified type."""
-        logger.debug(f"Loading dispatcher: {self.dispatcher}")
+        log_status("debug", None, f"Loading dispatcher: {self.dispatcher}")
         if self.dispatcher not in DispatcherType:
-            message = f"{self.dispatcher.upper()} not found in DispatcherType"
-            logger.error(message)
-            raise DispatcherError(message)
+            log_status(
+                "error",
+                DispatcherError,
+                f"{self.dispatcher.upper()} not found in DispatcherType",
+            )
         return self._load_plugin_dispatcher(
             dispatcher_type=self.dispatcher,
             obj=self.object_processor,
@@ -39,17 +41,20 @@ class PluginDispatcherAdapter:
         self, dispatcher_type: str, obj: dict
     ) -> object:
         """Load a registered plugin dispatcher based on the dispatcher type."""
-        logger.debug(f"Loading plugin dispatcher for type: {dispatcher_type}")
+        log_status(
+            "debug",
+            None,
+            f"Loading plugin dispatcher for type: {dispatcher_type}",
+        )
         valid_dispatcher = DispatcherType(dispatcher_type)
         dispatcher_class = Plugin.DISPATCHER[valid_dispatcher]
 
         if dispatcher_class is None:
-            message = (
+            log_status(
+                "error",
+                DispatcherError,
                 f"No plugin dispatcher registered for: {valid_dispatcher!r}",
             )
-            logger.error(message)
-            raise DispatcherError(message)
-
         return dispatcher_class(obj)
 
 
